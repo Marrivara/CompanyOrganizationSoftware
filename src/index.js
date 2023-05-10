b82a9eac2ab9a0859c49953c8aa51c3594d7e52e
Marrivara <berkaydulgerbaki@gmail.com>
2023-05-10 23:42:17 +0300

    update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './App.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode >
-    <div  style={{  minHeight: '100vh' }}>
+    <div  style={{ backgroundColor: '#E8F0FE', minHeight: '100vh' }}>
       <Page/>
     </div>
     
